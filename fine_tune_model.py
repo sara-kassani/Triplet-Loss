2317da40dfc69286ff1cb79ecc137d2d4b524f72
Travis Zhang <traviszhang@Traviss-MacBook-Pro.local>
2019-03-20 14:05:51 -0700

    Update

--- fine_tune_model.py
+++ fine_tune_model.py
@@ -18,147 +18,147 @@ def specificity(y_true, y_pred):
     possible_negatives = K.sum(K.round(K.clip(1-y_true, 0 ,1)))
     return true_negatives / (possible_negatives + K.epsilon())
 
-def original_resnet152(num_classes, shape=(224,224,3):
+def original_resnet152(num_classes, shape=(224,224,3)):
     initial_model = ResNet152(include_top=False, input_shape=shape)
-	last = initial_model.output
-	#print(last.shape)
-	x = Flatten()(last)
-	x = Dense(num_classes, activation='softmax')(x)
-	model = Model(initial_model.input, x)
+    last = initial_model.output
+    #print(last.shape)
+    x = Flatten()(last)
+    x = Dense(num_classes, activation='softmax')(x)
+    model = Model(initial_model.input, x)
 
-	#Train only higher layers to avoid overfitting
-	for layer in model.layers[:15]:
-		layer.trainable = False
+    #Train only higher layers to avoid overfitting
+    for layer in model.layers[:15]:
+        layer.trainable = False
 
     #Learning rate is changed to 0.001
-	adam = optimizers.adam(lr = 0.00146)
-	model.compile(optimizer=adam, loss="categorical_crossentropy", metrics=[sensitivity, specificity])
+    adam = optimizers.adam(lr = 0.00146)
+    model.compile(optimizer=adam, loss="categorical_crossentropy", metrics=[sensitivity, specificity])
 
-	return model
+    return model
 
-def original_resnet50(num_classes, shape=(224,224,3):
+def original_resnet50(num_classes, shape=(224,224,3)):
     initial_model = ResNet50(include_top=False, input_shape=shape)
-	last = initial_model.output
-	#print(last.shape)
-	x = Flatten()(last)
-	x = Dense(num_classes, activation='softmax')(x)
-	model = Model(initial_model.input, x)
+    last = initial_model.output
+    #print(last.shape)
+    x = Flatten()(last)
+    x = Dense(num_classes, activation='softmax')(x)
+    model = Model(initial_model.input, x)
 
-	#Train only higher layers to avoid overfitting
-	for layer in model.layers[:15]:
-		layer.trainable = False
+    #Train only higher layers to avoid overfitting
+    for layer in model.layers[:15]:
+        layer.trainable = False
 
     #Learning rate is changed to 0.001
-	adam = optimizers.adam(lr = 0.00146)
-	model.compile(optimizer=adam, loss="categorical_crossentropy", metrics=[sensitivity, specificity])
+    adam = optimizers.adam(lr = 0.00146)
+    model.compile(optimizer=adam, loss="categorical_crossentropy", metrics=[sensitivity, specificity])
 
-	return model
+    return model
                        
 def fine_tuned_resnet50(num_classes, shape=(224,224,3)):
 
-	'''
-		Inputs:
-					shape: The shape of the input tensor. Defaults to the tuple (3,224,224)
-					num_classes: The new number of classes for classification. 
+    '''
+        Inputs:
+                    shape: The shape of the input tensor. Defaults to the tuple (3,224,224)
+                    num_classes: The new number of classes for classification. 
 
-		Returns:
-					the fine tuned model.
+        Returns:
+                    the fine tuned model.
 
-		Functions:
-					--Initializes ResNet50 model without the classification layer and replaces
-					it with a new classifier with the appropriate number of classes.
-					--Freezes the first 15 layers of ResNet50 to avoid overfitting.
-					--Reduces learning rate for fine tuning.
-					--Compiles with triplet-loss.
-					--Any pre-trained model in the keras library can be used with the top off. 
-	'''
+        Functions:
+                    --Initializes ResNet50 model without the classification layer and replaces
+                    it with a new classifier with the appropriate number of classes.
+                    --Freezes the first 15 layers of ResNet50 to avoid overfitting.
+                    --Reduces learning rate for fine tuning.
+                    --Compiles with triplet-loss.
+                    --Any pre-trained model in the keras library can be used with the top off. 
+    '''
 
 
-	initial_model = ResNet50(include_top=False, input_shape=shape)
-	last = initial_model.output
-	#print(last.shape)
-	x = Flatten()(last)
-	x = Dense(num_classes, activation='softmax')(x)
-	model = Model(initial_model.input, x)
+    initial_model = ResNet50(include_top=False, input_shape=shape)
+    last = initial_model.output
+    #print(last.shape)
+    x = Flatten()(last)
+    x = Dense(num_classes, activation='softmax')(x)
+    model = Model(initial_model.input, x)
 
-	#Train only higher layers to avoid overfitting
-	for layer in model.layers[:15]:
-		layer.trainable = False
+    #Train only higher layers to avoid overfitting
+    for layer in model.layers[:15]:
+        layer.trainable = False
 
     #Learning rate is changed to 0.001
-	adam = optimizers.adam(lr = 0.00146)
-	model.compile(optimizer=adam, loss=triplet_loss_func, metrics=[sensitivity, specificity])
+    adam = optimizers.adam(lr = 0.00146)
+    model.compile(optimizer=adam, loss=triplet_loss_func, metrics=[sensitivity, specificity])
 
-	return model
+    return model
 
 def fine_tuned_resnet152(num_classes, shape=(224,224,3)):
 
-	'''
-		Inputs:
-					shape: The shape of the input tensor. Defaults to the tuple (3,224,224)
-					num_classes: The new number of classes for classification. 
+    '''
+        Inputs:
+                    shape: The shape of the input tensor. Defaults to the tuple (3,224,224)
+                    num_classes: The new number of classes for classification. 
 
-		Returns:
-					the fine tuned model.
+        Returns:
+                    the fine tuned model.
 
-		Functions:
-					--Initializes ResNet50 model without the classification layer and replaces
-					it with a new classifier with the appropriate number of classes.
-					--Freezes the first 15 layers of ResNet50 to avoid overfitting.
-					--Reduces learning rate for fine tuning.
-					--Compiles with triplet-loss.
-					--Any pre-trained model in the keras library can be used with the top off. 
-	'''
+        Functions:
+                    --Initializes ResNet50 model without the classification layer and replaces
+                    it with a new classifier with the appropriate number of classes.
+                    --Freezes the first 15 layers of ResNet50 to avoid overfitting.
+                    --Reduces learning rate for fine tuning.
+                    --Compiles with triplet-loss.
+                    --Any pre-trained model in the keras library can be used with the top off. 
+    '''
 
 
-	initial_model = ResNet152(include_top=False, input_shape=shape)
-	last = initial_model.output
-	#print(last.shape)
-	x = Flatten()(last)
-	x = Dense(num_classes, activation='softmax')(x)
-	model = Model(initial_model.input, x)
+    initial_model = ResNet152(include_top=False, input_shape=shape)
+    last = initial_model.output
+    #print(last.shape)
+    x = Flatten()(last)
+    x = Dense(num_classes, activation='softmax')(x)
+    model = Model(initial_model.input, x)
 
-	#Train only higher layers to avoid overfitting
-	for layer in model.layers[:15]:
-		layer.trainable = False
+    #Train only higher layers to avoid overfitting
+    for layer in model.layers[:15]:
+        layer.trainable = False
 
     #Learning rate is changed to 0.001
-	adam = optimizers.adam(lr = 0.00146)
-	model.compile(optimizer=adam, loss=triplet_loss_func, metrics=[sensitivity, specificity])
+    adam = optimizers.adam(lr = 0.00146)
+    model.compile(optimizer=adam, loss=triplet_loss_func, metrics=[sensitivity, specificity])
 
-	return model
+    return model
 
 def data_generator(sess,data,labels):
-	'''
-	This is not used.
-		
-		Inputs: 
-					sess: Instance of the session already initialised in the parent file.
-					data: data on which the model has to be trained.
-					labels: classes in the classification layer. (y_true)
+    '''
+    This is not used.
 
-		Returns:
-					Returns the generator function.
+        Inputs: 
+                    sess: Instance of the session already initialised in the parent file.
+                    data: data on which the model has to be trained.
+                    labels: classes in the classification layer. (y_true)
 
-		Functions:
-					--Resizes and preprocesses images in batches of 16.
-					--Yields batches of final images + batches of labels.
-					--Used as the generator function in the .fit_generator method.
-	'''
-	def generator():
-		batch_size = 16
-		start = 0
-		end = start + batch_size
-		n = data.shape[0]
-		while True:
-			batch_of_images_resized = sess.run(tf_resize_op, {batch_of_images_placeholder: data[start:end]})
-			batch_of_images_preprocessed = preprocess_input(batch_of_images_resized)
-			batch_of_labels = labels[start:end]
-            
-			start += batch_size
-			end   += batch_size
-			if start >= n:
-				start = 0
-				end = batch_size
-				yield (batch_of_images_preprocessed, batch_of_labels)
-		return generator
+        Returns:
+                    Returns the generator function.
+
+        Functions:
+                    --Resizes and preprocesses images in batches of 16.
+                    --Yields batches of final images + batches of labels.
+                    --Used as the generator function in the .fit_generator method.
+    '''
+    def generator():
+        batch_size = 16
+        start = 0
+        end = start + batch_size
+        n = data.shape[0]
+        while True:
+            batch_of_images_resized = sess.run(tf_resize_op, {batch_of_images_placeholder: data[start:end]})
+            batch_of_images_preprocessed = preprocess_input(batch_of_images_resized)
+            batch_of_labels = labels[start:end]
+
+            start += batch_size
+            end   += batch_size
+            if start >= n:
+                start = 0
+                end = batch_size
+                yield (batch_of_images_preprocessed, batch_of_labels)
+        return generator
--- init.py
+++ init.py
@@ -11,18 +11,18 @@ from misc_utils.filename_utils import get_log_filename
 import sys
 
 if __name__ == '__main__':
-	img_rows, img_cols = 224, 224
-	channels = 3
-	num_classes = 7 
-	batch_size = 32 
-	epochs = 50
+    img_rows, img_cols = 224, 224
+    channels = 3
+    num_classes = 7 
+    batch_size = 32 
+    epochs = 150
 
-	#Replace with your own dataset.
-	(x_train, y_train), (x_test, y_test) = load_training_data(task_idx=3,
+    #Replace with your own dataset.
+    (x_train, y_train), (x_test, y_test) = load_training_data(task_idx=3,
                                                                    output_size=224,
                                                                    num_partitions=1,
                                                                     test_split = 0.8)
-	
+
     class_wt_type = 'ones'
     run_name = 'ResNet152'
 
@@ -36,49 +36,49 @@ if __name__ == '__main__':
     class_weights = compute_class_weights(y_train, wt_type=class_wt_type)
 
     sys.stdout.flush()
-	"""s#Size of training batch
-	batch_train = x_train.shape[0]
-	#Size of testing batch
-	batch_test = x_test.shape[0]
+    """s#Size of training batch
+    batch_train = x_train.shape[0]
+    #Size of testing batch
+    batch_test = x_test.shape[0]
 
-	y_train = y_train.flatten()
-	y_test  = y_test.flatten()
+    y_train = y_train.flatten()
+    y_test  = y_test.flatten()
 
-	#Uncomment this to reshape images to a size acceptable by the CNN
-	
-	x_train = x_train.reshape(batch_size, channels, img_rows, img_cols)
-	x_test = x_test.reshape(batch_size, channels, img_rows, img_cols)
-	
+    #Uncomment this to reshape images to a size acceptable by the CNN
 
-	x_train = x_train.astype('float32')
-	x_test = x_test.astype('float32')
+    x_train = x_train.reshape(batch_size, channels, img_rows, img_cols)
+    x_test = x_test.reshape(batch_size, channels, img_rows, img_cols)
 
-	#-----------------------------------------------------------------------------
-	#Initialized tensorflow session.
-	'''
-	sess = tf.InteractiveSession()
-	K.set_session(sess)
-	'''"""
-	#-----------------------------------------------------------------------------
 
-	#10 classes because dataset is Cifar-10
-	model = original_resnet152(7)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+
+    #-----------------------------------------------------------------------------
+    #Initialized tensorflow session.
+    '''
+    sess = tf.InteractiveSession()
+    K.set_session(sess)
+    '''"""
+    #-----------------------------------------------------------------------------
+
+    #10 classes because dataset is Cifar-10
+    model = original_resnet152(7)
     #model = original_resnet50(7)
-	
-	#-----------------------------------------------------------------------------
-	#fit_generator is a method that fits the model on data that is 
-	#processed in batches before being sent to the model 
-	'''
-	#create one-hot encodings of the true image classes
-	y_train_one_hot = tf.one_hot(y_train, num_classes).eval()
-	data_train_gen = data_generator(sess, x_train, y_train_one_hot)
+
+    #-----------------------------------------------------------------------------
+    #fit_generator is a method that fits the model on data that is 
+    #processed in batches before being sent to the model 
+    '''
+    #create one-hot encodings of the true image classes
+    y_train_one_hot = tf.one_hot(y_train, num_classes).eval()
+    data_train_gen = data_generator(sess, x_train, y_train_one_hot)
     # Fit model on data using fit_generator
-	model.fit_generator(data_train_gen(), epochs=batch_train/batch_size, verbose=1)
-	'''
-	#-------------------------------------------------------------------------------
-	
-	#Fit model on the training and testing datasets
-	model.fit(x=x_train,
+    model.fit_generator(data_train_gen(), epochs=batch_train/batch_size, verbose=1)
+    '''
+    #-------------------------------------------------------------------------------
+
+    #Fit model on the training and testing datasets
+    model.fit(x=x_train,
               y=y_train,
               batch_size=batch_size,
               epochs=epochs,
@@ -90,4 +90,4 @@ if __name__ == '__main__':
               )
 
     # Make predictions
-	predictions_valid = model.predict(x_test, batch_size=batch_size, verbose=1)
+    predictions_valid = model.predict(x_test, batch_size=batch_size, verbose=1)
