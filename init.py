1494930f53c42e4f7f79372726b16d792ffa0d89
zhangtravis <popcorn20020806@gmail.com>
2019-03-31 15:23:17 -0700

    Update init.py

--- init.py
+++ init.py
@@ -5,7 +5,7 @@ os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
 os.environ["CUDA_VISIBLE_DEVICES"]="0";  
 from fine_tune_model import original_resnet152, original_resnet50
 from keras.preprocessing.image import ImageDataGenerator
-from load_data import load_training_data, load_task3_training_labels
+from load_data import load_full_data, load_task3_training_labels
 import numpy as np
 import tensorflow as tf
 from keras import backend as K
@@ -14,20 +14,32 @@ from misc_utils.eval_utils import compute_class_weights
 from misc_utils.print_utils import log_variable, Tee
 from misc_utils.filename_utils import get_log_filename
 import sys
+from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
     img_rows, img_cols = 224, 224
     channels = 3
     num_classes = 7 
     batch_size = 16
-    epochs = 300
+    epochs = 250
     
+    print("Loading Images...")
+    x = load_full_data()
     y = load_task3_training_labels()
     #Replace with your own dataset.
+    
+    print("Splitting Data...")
+    x_train, x_valid, y_train, y_valid = train_test_split(x, y, test_size=0.3, random_state=42)
+    
+    print("x_train shape: ", x_train.shape)
+    print("y_train shape: ", y_train.shape)
+    print("x_valid shape: ", x_valid.shape)
+    print("y_valid shape: ", y_valid.shape)
+    """
     (x_train, y_train), (x_valid, y_valid), _ = load_training_data(task_idx=3,
                                                                    output_size=224,
                                                                    num_partitions=1,
-                                                                    test_split = 0.8)
+                                                                    test_split = 0.8)"""
 
     class_wt_type = 'balanced'
     run_name = 'OriginalResNet152'
@@ -35,6 +47,8 @@ if __name__ == '__main__':
     num_classes = y_train.shape[1]
     
     class_weights = compute_class_weights(y, wt_type=class_wt_type)
+    
+    print(class_weights)
     
     callbacks = config_cls_callbacks(run_name)
 
